78b700875f60b466639194381384c4b234e2e9b9
Jagadeesh Kovi <jgdshkovi@gmail.com>
2025-04-26 20:06:44 -0400

    39vh - loading screen

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const LoadingScreen = () => {
   return (
     <div className="loading-container">
-      <div className="logo-wrapper md:pl-8 lg:pl-12 flex items-start justify-start mt-[30vh]">
+      <div className="logo-wrapper md:pl-8 lg:pl-12 flex items-start justify-start mt-[39vh]">
         <div className="logo-line"></div>
         <div className="letter">Jagadeesh Kovi</div>
       </div>
@@ -13,3 +13,21 @@ const LoadingScreen = () => {
 };
 
 export default LoadingScreen;
+
+
+// import React from 'react';
+
+// const LoadingScreen = () => {
+//   return (
+//     <div className="loading-container">
+//       <div className="min-h-screen flex flex-col justify-center relative">
+//         <div className="container mx-auto px-6 md:px-8 lg:px-12 flex flex-col items-start justify-center">
+//           <div className="logo-line"></div>
+//           <div className="letter">Jagadeesh Kovi</div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default LoadingScreen;
